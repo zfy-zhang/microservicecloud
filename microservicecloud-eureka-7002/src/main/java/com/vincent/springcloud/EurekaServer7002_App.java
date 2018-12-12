68f994d51e6da28913d0347289827809460d1e82
zfy-zhang <zfy_zang@163.com>
2018-12-12 15:25:47 +0800

    eureka与ribbon实现负载均衡

---
+++ microservicecloud-eureka-7002/src/main/java/com/vincent/springcloud/EurekaServer7002_App.java
@@ -0,0 +1,20 @@
+package com.vincent.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created with IDEA
+ * author:vincent
+ * Date:2018/12/12
+ */
+@SpringBootApplication
+@EnableEurekaServer // EurekaServer服务器端启动类,接受其它微服务注册进来
+public class EurekaServer7002_App
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(EurekaServer7002_App.class, args);
+    }
+}
